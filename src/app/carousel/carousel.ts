72bc4946565ea31dad7b33e5855422ea25da1c1d
Akshaya A <akshayaajith2006@gmail.com>
2026-06-26 13:37:14 +0530

    Initial commit

---
+++ src/app/carousel/carousel.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carousel',
+  standalone: true,
+  templateUrl: './carousel.html',
+  styleUrl: './carousel.css'
+})
+export class Carousel {}
